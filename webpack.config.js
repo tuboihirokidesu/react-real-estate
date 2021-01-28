3d35bb2aec72f9681ad948fc8c0dff1a949d57d9
坪井浩紀 <tsuboihiroki@tsuboihirokinoMacBook-ea.local>
2021-01-28 22:54:57 +0900

    React環境構築

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: "./src/index.tsx", //②
+  devtool: "eval-source-map", //devtoolでエラー箇所を正確に
+  resolve: {
+    extensions: [".js", ".ts", ".tsx"],
+  }, //import export を使用可能にしてくれる。
+  module: {
+    rules: [
+      //③
+      {
+        test: /\.tsx?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          { loader: "css-loader", options: { modules: true } },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    //①
+    new HtmlWebpackPlugin({
+      //htmlファイルにscriptタグにjsファイルを指定しなくて良いための機能。distに自動的にscriptタグが生成される。
+      template: "./src/index.html",
+      filename: "index.html",
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+};
